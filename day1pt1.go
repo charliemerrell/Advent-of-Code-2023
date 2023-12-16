6ba9160da84e49c589f741b520d1ce333e3988cb
Charlie Merrell <charlie.merrell@enable.com>
2023-12-16 19:58:22 +0000

    refactor

--- day1pt1.go
+++ day1pt1.go
@@ -12,20 +12,25 @@ func Day1Pt1() int {
 	sum := 0
 	for i := 0; i < len(lines); i++ {
 		line := lines[i]
-		digitStart := '_'
-		digitEnd := '0'
-		for j := 0; j < len(line); j++ {
-			c, _ := utf8.DecodeRuneInString(line[j:])
-			if unicode.IsDigit(c) {
-				if digitStart == '_' {
-					digitStart = c
-				}
-				digitEnd = c
-			}
-		}
-		bothDigits := string(digitStart) + string(digitEnd)
-		num, _ := strconv.Atoi(bothDigits)
+		num := getLineSum(line)
 		sum += num
 	}
 	return sum
+}
+
+func getLineSum(line string) int {
+	digitStart := '_'
+	digitEnd := '0'
+	for j := 0; j < len(line); j++ {
+		c, _ := utf8.DecodeRuneInString(line[j:])
+		if unicode.IsDigit(c) {
+			if digitStart == '_' {
+				digitStart = c
+			}
+			digitEnd = c
+		}
+	}
+	bothDigits := string(digitStart) + string(digitEnd)
+	num, _ := strconv.Atoi(bothDigits)
+	return num
 }
--- day1pt2.go
+++ day1pt2.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"aoc2023/lines"
-	"fmt"
 	"unicode"
 	"unicode/utf8"
 )
@@ -13,32 +12,36 @@ func Day1Pt2() int {
 	numbers := map[string]int{"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
 	for i := 0; i < len(lines); i++ {
 		line := lines[i]
-		digitStart := -1
-		digitEnd := 0
-		for j := 0; j < len(line); j++ {
-			c, _ := utf8.DecodeRuneInString(line[j:])
-			if unicode.IsDigit(c) {
-				if digitStart == -1 {
-					digitStart = int(c - '0')
-				}
-				digitEnd = int(c - '0')
-			} else {
-				for numStr, numInt := range numbers {
-					if j+len(numStr) <= len(line) {
-						chars := line[j:j+len(numStr)]
-						if chars == numStr {
-							if digitStart == -1 {
-								digitStart = numInt
-							}
-							digitEnd = numInt
+		inc := newFunction(line, numbers)
+		sum += inc
+	}
+	return sum
+}
+
+func newFunction(line string, numbers map[string]int) int {
+	digitStart := -1
+	digitEnd := 0
+	for j := 0; j < len(line); j++ {
+		c, _ := utf8.DecodeRuneInString(line[j:])
+		if unicode.IsDigit(c) {
+			if digitStart == -1 {
+				digitStart = int(c - '0')
+			}
+			digitEnd = int(c - '0')
+		} else {
+			for numStr, numInt := range numbers {
+				if j+len(numStr) <= len(line) {
+					chars := line[j : j+len(numStr)]
+					if chars == numStr {
+						if digitStart == -1 {
+							digitStart = numInt
 						}
+						digitEnd = numInt
 					}
 				}
 			}
 		}
-		inc := (digitStart * 10) + digitEnd
-		fmt.Println(line, ": ", inc)
-		sum += inc
 	}
-	return sum
+	inc := (digitStart * 10) + digitEnd
+	return inc
 }
