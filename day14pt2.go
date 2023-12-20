61b6bef88692da79bfc8c2f3aa6d07fb37a00eb2
Charlie Merrell <charlie.merrell@enable.com>
2023-12-20 14:35:29 +0000

    Pt 2 wip

---
+++ day14pt2.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"aoc2023/lines"
+	"fmt"
+)
+
+func Day14Pt2() int {
+	lines := lines.GetLines("./day14.txt")
+	grid := make([][]byte, len(lines))
+	for y := range lines {
+		grid[y] = make([]byte, len(lines[y])) 
+		for x := range lines[y] {
+			grid[y][x] = lines[y][x]
+		}
+	}
+
+	sum := 0
+	for y := range grid {
+		for x := range grid[y] {
+			if grid[y][x] == 'O' {
+				for newY := y - 1; newY >= 0 && grid[newY][x] == '.'; newY-- {
+					grid[newY+1][x] = '.'
+					grid[newY][x] = 'O'
+				}
+			}
+		}
+	}
+	printGrid(grid)
+
+	for y := range grid {
+		for x := range grid[y] {
+			if grid[y][x] == 'O' {
+				sum += len(grid) - y
+			}
+		}
+	}
+	return sum
+}
+
+func rotateRight90(grid [][]int) {
+	n 
+	for y := range grid {
+		for x := range grid[y] {
+			tmp := grid[y][x];
+			grid[y][x]=grid[x][n-y-1];
+			grid[x][n-y-1]=grid[n-y-1][n-x-1];
+			grid[n-y-1][n-x-1]=grid[n-x-1][y];
+			grid[n-x-1][y]=tmp;
+		}
+	}
+}
